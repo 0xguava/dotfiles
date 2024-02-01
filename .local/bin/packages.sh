b4c56c2a05f906ad56958d21c35e9bbba794a0fa
0xguava <demo.lappy@protonmail.com>
2024-02-01 18:38:21 +0530

    post installation package file added

---
+++ .local/bin/packages.sh
@@ -0,0 +1,9 @@
+pacman -S base-devel git libx11 libxft firefox sxiv xclip xsel xf86-input-synaptics \
+  qrencode xf86-video-intel xorg-server xorg-xinit xwallpaper mpv ranger ufw vnstat zsh \
+  zsh-completions zsh-syntax-highlighting pipewire pipewire-audio pipewire-pulse\
+  npm nodejs bluez bluez-utils brightnessctl cmake fzf maim man-db man-pages mlocate mpc \
+  dunst aria2 mpd ncmpcpp tmux noto-fonts-emoji picom python python-pip python-pywal \
+  python-setuptools qbittorrent telegram-desktop ttf-jetbrains-mono ttf-nerd-fonts-symbols-common \
+  ttf-nerd-fonts-symbols-mono ueberzug usbutils virtualbox virtualbox-guest-utils wget yt-dlp\
+  zathura zathura-pdf-mupdf
+#yay -S python-pywalfox sioyek-git wal-telegram-git cava 
