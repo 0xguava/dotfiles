8d83eea6f99d7ce9e06c28c31006c987d9724df5
0xguava <demo.lappy@protonmail.com>
2024-02-02 11:59:59 +0530

    added

--- .local/bin/packages.sh
+++ .local/bin/packages.sh
@@ -1,6 +1,6 @@
 pacman -S base-devel git libx11 libxft firefox sxiv xclip xsel xf86-input-synaptics \
   qrencode xf86-video-intel xorg-server xorg-xinit xwallpaper mpv ranger ufw vnstat zsh \
-  zsh-completions zsh-syntax-highlighting pipewire pipewire-audio pipewire-pulse\
+  libnotify zsh-completions zsh-syntax-highlighting pipewire pipewire-audio pipewire-pulse\
   npm nodejs bluez bluez-utils brightnessctl cmake fzf maim man-db man-pages mlocate mpc \
   dunst aria2 mpd ncmpcpp tmux noto-fonts-emoji picom python python-pip python-pywal \
   python-setuptools qbittorrent telegram-desktop ttf-jetbrains-mono ttf-nerd-fonts-symbols-common \
