6b534cfc28cb609b255f60ccad32cf493a57dbf3
0xguava <demo.lappy@protonmail.com>
2025-02-06 17:13:10 +0530

    cleaned

--- .local/bin/packages.sh
+++
@@ -1,9 +0,0 @@
-pacman -S base-devel git libx11 libxft firefox sxiv xclip xsel xf86-input-synaptics \
-  qrencode xf86-video-intel xorg-server xorg-xinit xwallpaper mpv ranger ufw vnstat zsh \
-  libnotify zsh-completions zsh-syntax-highlighting pipewire pipewire-audio pipewire-pulse\
-  npm nodejs bluez bluez-utils brightnessctl cmake fzf maim man-db man-pages mlocate mpc \
-  htop dunst aria2 mpd ncmpcpp tmux noto-fonts-emoji picom python python-pip python-pywal \
-  python-setuptools qbittorrent telegram-desktop ttf-jetbrains-mono ttf-nerd-fonts-symbols-common \
-  ttf-nerd-fonts-symbols-mono ueberzug usbutils virtualbox virtualbox-guest-utils wget yt-dlp\
-  zathura zathura-pdf-mupdf xorg-xrandr unzip openssh
-#yay -S python-pywalfox sioyek-git wal-telegram-git cava 
